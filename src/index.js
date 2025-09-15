7c1a1b186390966051e8493f52d4a6fc774f2984
Yeryeong Kang <dpfud8786@gmail.com>
2025-09-15 15:47:48 +0900

    refactor: judgeResult 함수 else 생략

--- src/index.js
+++ src/index.js
@@ -65,15 +65,10 @@ function scoreStrikeBall(computerInput, userInput) {
 */
 
 function judgeResult(ball, strike) {
-  if (ball === 0 && strike === 0) {
-    return "낫싱";
-  } else if (ball > 0 && strike > 0) {
-    return `${ball}볼 ${strike}스트라이크`;
-  } else if (ball > 0) {
-    return `${ball}볼`;
-  } else {
-    return `${strike}스트라이크`;
-  }
+  if (ball === 0 && stirke === 0) return "낫싱";
+  if (ball > 0 && strike > 0) return `${ball}볼 ${strike}스트라이크`;
+  if (ball > 0) return `${ball}볼`;
+  return `${strike}스트라이크`;
 }
 
 /* 기능 구현 4. 랜덤 숫자 생성 기능
