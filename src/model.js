7498f08cc4a0c686a9f8295a1933d5326260daba
Yeryeong Kang <dpfud8786@gmail.com>
2025-09-19 22:31:02 +0900

    refactor: MVC 패턴 적용 - Model

---
+++ src/model.js
@@ -0,0 +1,51 @@
+export default function BaseballGame() {
+  this.play = function (computerInputNumbers, userInputNumbers) {
+    const computerInput = String(computerInputNumbers).trim();
+    const userInput = String(userInputNumbers).trim();
+
+    if (!isValidInput(userInput)) return "";
+    const { ball, strike } = scoreStrikeBall(computerInput, userInput);
+    return judgeResult(ball, strike);
+  };
+}
+
+export const DIGIT_LENGTH = 3;
+
+export function isValidInput(userInput, digitLen = DIGIT_LENGTH) {
+  const userInputString = String(userInput).trim();
+
+  if (userInputString.length !== digitLen) return false;
+  if ([...userInputString].some((ch) => ch < "1" || ch > "9")) return false;
+  if (new Set(userInputString).size !== userInputString.length) return false;
+  return true;
+}
+
+export function scoreStrikeBall(computerInput, userInput) {
+  let ball = 0;
+  let strike = 0;
+
+  for (let i = 0; i < DIGIT_LENGTH; i++) {
+    if (userInput[i] === computerInput[i]) strike += 1;
+    else if (computerInput.includes(userInput[i])) ball += 1;
+  }
+  return { ball, strike };
+}
+
+export function judgeResult(ball, strike) {
+  if (ball === 0 && strike === 0) return "낫싱";
+  if (ball > 0 && strike > 0) return `${ball}볼 ${strike}스트라이크`;
+  if (ball > 0) return `${ball}볼`;
+  return `${strike}스트라이크`;
+}
+
+export function makeComputerInput() {
+  const correctAnswer = [];
+
+  while (correctAnswer.length < DIGIT_LENGTH) {
+    const digit = window.MissionUtils.Random.pickNumberInRange(1, 9);
+    if (!correctAnswer.includes(digit)) {
+      correctAnswer.push(digit);
+    }
+  }
+  return correctAnswer.join("");
+}
